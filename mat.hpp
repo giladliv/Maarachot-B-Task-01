2708673fb309ac95b8a93f197bf76930bba46f33
giladliv <giladliv123@gmail.com>
2022-03-09 00:53:05 +0200

    the first part plus the second

--- mat.hpp
+++ mat.hpp
@@ -8,15 +8,18 @@ using std::string;
 using std::exception;
 
 namespace ariel
-{
+{   
+    class SizeException : public exception
+    {
+        public:
+            const char* what() const noexcept {return "Mat size is always odd";}
+    };
+    
     string mat(int cols, int rows, char a, char b);
     void rec_round_mat(int s_r, int s_c, int e_r, int e_c, string frame_chars, string mat[]);
 }
 
-class SizeException : public exception
-{
-    const char* what() const noexcept {return "Mat size is always odd";}
-};
+
 
 
 #endif /* MAT_H */
