31ff34b7281443034850e3b9663cb5c58cfad73c
giladliv <giladliv123@gmail.com>
2022-03-15 12:08:10 +0200

    updated

--- Test.cpp
+++ Test.cpp
@@ -77,7 +77,7 @@ TEST_CASE("Good input")
                                                     "GGGGGGGGG"));
 }
 
-TEST_CASE("Known structure")
+TEST_CASE("Good Letters")
 {
     string str_format = "\t\t\t\t\t\t\t\t\t\n"      // this is the sample of 7x9
                         "\t\r\r\r\r\r\r\r\t\n"      // the letters are the ones who won't be good for the input
